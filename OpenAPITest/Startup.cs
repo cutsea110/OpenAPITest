c7f8eb47242be590c5d359199160b177247ea77a
cutsea110 <cutsea110@gmail.com>
2019-08-02 13:39:36 +0900

    [Refactor] remove unnecessary import list

--- OpenAPITest/Startup.cs
+++ OpenAPITest/Startup.cs
@@ -1,27 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml.XPath;
 using LinqToDB.Configuration;
 using LinqToDB.Data;
-using LinqToDB.DataProvider.SqlServer;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
