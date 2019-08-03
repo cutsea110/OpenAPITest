0445889b6bdf9f7b505fd74a19ee4f6c8573c4a1
cutsea110 <cutsea110@gmail.com>
2019-08-03 13:41:59 +0900

    [FIX] forget to add files

---
+++ OpenAPITest/core/PermissionTypeHandler.cs
@@ -0,0 +1,59 @@
+﻿using LinqToDB;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using OpenAPITest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OpenAPITest.core
+{
+    /// <summary>
+    /// PermissionTypeRequirement要件ハンドラクラス
+    /// </summary>
+    public class PermissionTypeHandler : AuthorizationHandler<PermissionTypeRequirement>
+    {
+        IHttpContextAccessor _httpContextAccessor = null;
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="httpContextAccessor"></param>
+        public PermissionTypeHandler(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// PermissionTypeRequirement要件を実際に確認するハンドラ
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="requirement"></param>
+        /// <returns></returns>
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, PermissionTypeRequirement requirement)
+        {
+            HttpContext httpContext = _httpContextAccessor.HttpContext;
+
+            using (var db = new peppaDB())
+            {
+                // 現在の(認証済み)アカウントID
+                var currentAccountID = int.Parse(context.User.FindFirst(c => c.Type == ClaimTypes.Name).Value);
+                // 所持している権限リスト
+                var permissions =
+                    db.Account
+                    .LoadWith(_ => _.AccountRoleList.First().Role.RolePermissionList)
+                    .Where(_ => _.account_id == currentAccountID)
+                    .SelectMany(a => a.AccountRoleList.SelectMany(r => r.Role.RolePermissionList.Select(rp => rp.permission_id)))
+                    .Distinct();
+
+                if (requirement.Permissions.Select(_ => _.Val()).Intersect(permissions).Any())
+                {
+                    context.Succeed(requirement);
+                    return Task.CompletedTask;
+                }
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
---
+++ OpenAPITest/core/PermissionTypeRequirement.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using OpenAPITest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenAPITest.core
+{
+    /// <summary>
+    /// PermissionTypeによる独自ポリシーのための要件定義
+    /// </summary>
+    public class PermissionTypeRequirement : IAuthorizationRequirement
+    {
+        /// <summary>
+        /// 権限リスト
+        /// </summary>
+        public PermissionType[] Permissions { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="permissions"></param>
+        public PermissionTypeRequirement(PermissionType[] permissions)
+        {
+            Permissions = permissions;
+        }
+    }
+}
